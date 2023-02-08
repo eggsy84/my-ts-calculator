72a32269043944708e146d1112dfc54a43ad5d1e
James Heggs <james.heggs@techreturners.com>
2023-02-08 11:00:00 +0000

    feat: Introducing ability to run application

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -1,4 +1,4 @@
-import { sum } from "../src/calc";
+import { sum } from "./calc";
 
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { sum } from "./calc";
+
+console.log("Hello everyone");
+
+console.log(`The answer to 10 + 5 is ${sum(10, 5)}`);
